3792646b649e2b53883b25ec70ed9dc4e9b0e41e
Christopher Evan <christopher.evan0603@gmail.com>
2022-10-06 22:29:59 +0700

    Update hitung.py

--- hitung.py
+++ hitung.py
@@ -21,45 +21,32 @@ def evaluate(xl, xu, xr, f_xl, f_xr):
 def calculateEa(xr, xr_prev):
     return float((abs((xr - xr_prev)/xr)*100))
 
-def checkPolarity(f_xl, f_xu, f_xr):
-    xl = "+"
-    xu = "+"
-    xr = "+"
+# def hitung(xr, persamaan_xl, persamaan_xu, persamaan_xr,):
+#     xr_prev = xr
+#     xr = float((xl+xu)/2)
+#     f_xl = float(eval(persamaan_xl))
+#     f_xu = float(eval(persamaan_xu))
+#     f_xr = float(eval(persamaan_xr))
 
-    if (f_xl < 0):
-        xl = "-"
-    if (f_xu < 0):
-        xu = "-"
-    if (f_xr < 0):
-        xr = "-"
+#     if (i != 0):
+#         ea = calculateEa(xr, xr_prev)
 
-    return xl, xu, xr
+#     print_ea = ea
+#     if (i == 0):
+#         print_ea = '-'
 
-def hitung():
-    xr_prev = xr
-    xr = float((xl+xu)/2)
-    f_xr = float(eval(persamaan_xr))
+#     row = [{'iterasi': i+1, 'xl': xl, 'xu': xu, 'xr': xr, 'f(xl)': f_xl, 'f(xu)': f_xu, 'f(xr)': f_xr, '|ea|%': print_ea}]
+#     df = pd.DataFrame.from_dict(row)
+#     result = pd.concat([result, df])
 
-    if (i != 0):
-        ea = calculateEa(xr, xr_prev)
-
-    p_xl, p_xu, p_xr = checkPolarity(f_xl, f_xu, f_xr)
-    print_ea = ea
-    if (i == 0):
-        print_ea = '-'
-
-    row = [{'iterasi': i+1, 'xl': xl, 'xu': xu, 'xr': xr, 'f(xl)': p_xl, 'f(xu)': p_xu, 'f(xr)': p_xr, '|ea|%': print_ea}]
-    df = pd.DataFrame.from_dict(row)
-    result = pd.concat([result, df])
-
-    impact, new_xl, new_xu, new_xr = evaluate(xl, xu, xr, f_xl, f_xr)
-    if (impact == "xl"):
-        xl = xr
-    elif (impact == "xu"):
-        xu = xr
-    else:
-        root = new_xl
-        # break
+#     impact, new_xl, new_xu, new_xr = evaluate(xl, xu, xr, f_xl, f_xr)
+#     if (impact == "xl"):
+#         xl = xr
+#     elif (impact == "xu"):
+#         xu = xr
+#     else:
+#         root = new_xl
+#         break
 
 persamaan = sys.argv[1]
 # tex_persamaan = os.system("converter.py " + persamaan)
@@ -106,7 +93,6 @@ if (f_xl_xu < 0):
             if (i != 0):
                 ea = calculateEa(xr, xr_prev)
 
-            p_xl, p_xu, p_xr = checkPolarity(f_xl, f_xu, f_xr)
             print_ea = ea
             if (i == 0):
                 print_ea = '-'
@@ -142,7 +128,6 @@ if (f_xl_xu < 0):
             if (i != 0):
                 ea = calculateEa(xr, xr_prev)
 
-            p_xl, p_xu, p_xr = checkPolarity(f_xl, f_xu, f_xr)
             print_ea = ea
             if (i == 0):
                 print_ea = '-'
@@ -177,7 +162,6 @@ if (f_xl_xu < 0):
             if (i != 0):
                 ea = calculateEa(xr, xr_prev)
 
-            p_xl, p_xu, p_xr = checkPolarity(f_xl, f_xu, f_xr)
             print_ea = ea
             if (i == 0):
                 print_ea = '-'
@@ -198,8 +182,6 @@ if (f_xl_xu < 0):
             i += 1
         mystr = '|f(xr)| = ' + str(absFxr)
         metodeStop += mystr
-        # if (ea < es):
-        #     root = xr
     output = ""
      
     for idx, row in result.iterrows():
